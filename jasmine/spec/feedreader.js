e8b83586ed331b4f33358f6369f048a241e5e733
Debby Wasserstein <debby.was@gmail.com>
2018-09-21 17:33:44 -0400

    test to ensure that on loadFeed there is at least one entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -89,6 +89,8 @@ $(function() {
       });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+        describe("Initial Entries", function() {
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -96,7 +98,17 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+          //adding the beforeEach function for the very first index
+          beforeEach(function(done){
+            loadFeed(0, done);
+          });
 
+          it("completes work", function(){
+            const feed = document.querySelector(".feed");
+            expect(feed.children.length > 0).toBe(true);
+          });
+
+       });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
